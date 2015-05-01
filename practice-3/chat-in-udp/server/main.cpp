8982ace2654257e3c9cfe9ae9230c77689202667
Lucas David <lucas.david@drexel.edu>
2015-05-01 19:31:19 -0300

    Change protocol parameters

--- practice-3/chat-in-udp/server/main.cpp
+++ practice-3/chat-in-udp/server/main.cpp
@@ -6,30 +6,32 @@
 #define DEFAULT_PORT "5000"
 
 extern DWORD WINAPI thread_Servidor(LPVOID lpParameter);
-int main(void);
+extern void         cleanUp(); 
+int                 main(void);
 
-extern void cleanUp();
-
-void intHandler(int dummy)
+void
+intHandler(int dummy)
 {
 	cleanUp();
 }
 
-int main(void)
+int
+main(void)
 {
 	signal(SIGINT, intHandler);
-	SocketParams params;
+	
+    SocketParams params;
 
 	strcpy(params.port, DEFAULT_PORT);
 
 	params.family   = AF_INET;
-	params.socktype = SOCK_STREAM;
-	params.protocol = IPPROTO_TCP;
+	params.socktype = SOCK_DGRAM;
+	params.protocol = IPPROTO_UDP;
 	params.flags    = AI_PASSIVE;
 	
 	Servidor = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)thread_Servidor, &params, 0, 0);
 	
 	WaitForSingleObject(Servidor, INFINITE);
 
-	exit(0);
+    return 0;
 }
